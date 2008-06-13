650f087b5baf22105d201d5485996bd1ee7f534b
Esko Luontola <esko.luontola@gmail.com>
2008-06-13 15:42:22 +0000

    add: rotating tetriminoes / the O shape, is shaped like O

---
+++ src/test/java/tetris/RotatingTetrominoesTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2008, Esko Luontola. All Rights Reserved.
+ *
+ * This program and its source code may be freely used for personal
+ * non-commercial educational purposes. Usage as course material
+ * is not allowed without prior permission.
+ */
+
+package tetris;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * @author orfjackal
+ * @since Jun 13, 2008
+ */
+public class RotatingTetrominoesTest {
+
+    public static Test suite() {
+        return new TestSuite(RotatingTetrominoesTest.class.getDeclaredClasses());
+    }
+
+    public static class TheOShape extends TestCase {
+
+        private Tetrominoe shape;
+
+        protected void setUp() throws Exception {
+            shape = new Tetrominoe("" +
+                    ".YY\n" +
+                    ".YY\n" +
+                    "...\n");
+        }
+
+        public void testIsShapedLikeO() {
+            assertEquals("" +
+                    ".YY\n" +
+                    ".YY\n" +
+                    "...\n", shape.toString());
+
+        }
+    }
+}
