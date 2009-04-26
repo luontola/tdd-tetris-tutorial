6a3e52b15f2e986c3a11a338367dcc218edca92d
Esko Luontola <esko.luontola@gmail.com>
2009-04-26 17:49:34 +0300

    Prepare tutorial: Added base Board class to get tests to compile.

---
+++ src/main/java/tetris/Board.java
@@ -0,0 +1,23 @@
+package tetris;
+
+public class Board {
+
+    private final int rows;
+    private final int columns;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                s += "?";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+}
